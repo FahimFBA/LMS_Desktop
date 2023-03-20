f734703f8c731a80328f5f2abec2acd52052868d
FahimFBA <fahimbinamin@gmail.com>
2023-03-20 19:30:04 +0600

    Add: FacultyLanding GUI

---
+++ src/main/java/com/lms/lmsdesktop/FacultyLandingController.java
@@ -0,0 +1,34 @@
+package com.lms.lmsdesktop;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class FacultyLandingController {
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Text invalid;
+
+    @FXML
+    void AllCourseFaculty(MouseEvent event) {
+
+    }
+
+    @FXML
+    void Submit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void backFac(MouseEvent event) {
+
+    }
+
+    @FXML
+    void searchCourseFaculty(MouseEvent event) {
+
+    }
+}
