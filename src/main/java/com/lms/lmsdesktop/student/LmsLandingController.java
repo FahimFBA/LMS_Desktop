ecc07f766e82eac722f092e4f85af835cf8f8fc3
FahimFBA <fahimbinamin@gmail.com>
2023-03-20 22:00:49 +0600

    Add: `LmsLanding` GUI

---
+++ src/main/java/com/lms/lmsdesktop/student/LmsLandingController.java
@@ -0,0 +1,16 @@
+package com.lms.lmsdesktop.student;
+
+import com.lms.lmsdesktop.MainApplication;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LmsLandingController {
+    @FXML
+    private ImageView userImage;
+}
