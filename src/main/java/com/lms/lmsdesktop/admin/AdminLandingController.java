5b7486b3e741e0ea1515cb6730c8cf12eaa7ad2e
FahimFBA <fahimbinamin@gmail.com>
2023-03-23 11:08:58 +0600

    call `FXML` from a different directory using a button

--- src/main/java/com/lms/lmsdesktop/admin/AdminLandingController.java
+++ src/main/java/com/lms/lmsdesktop/admin/AdminLandingController.java
@@ -1,4 +1,36 @@
 package com.lms.lmsdesktop.admin;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AdminLandingController {
+    @FXML
+    private Button Home;
+
+    @FXML
+    void Home(ActionEvent event) throws IOException {
+        // Create a new FXMLLoader object by passing the path of the FXML file as a parameter to the constructor
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/lms/lmsdesktop/main-application-view.fxml"));
+
+        // Use the load() method of the FXMLLoader object to load the FXML file and create the corresponding UI component
+        Parent root = loader.load();
+
+        // Create a new Scene object with the loaded UI component
+        Scene scene = new Scene(root);
+
+        // Get the current stage (window)
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set the new scene to the stage and show it
+        stage.setScene(scene);
+        stage.show();
+    }
 }
