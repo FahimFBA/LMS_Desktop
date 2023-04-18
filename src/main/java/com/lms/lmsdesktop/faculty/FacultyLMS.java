8bf3e01b174455b3c2548b8da5eeab41c1dc3195
FahimFBA <fahimbinamin@gmail.com>
2023-04-19 01:46:23 +0600

    UI: till site home

---
+++ src/main/java/com/lms/lmsdesktop/faculty/FacultyLMS.java
@@ -0,0 +1,11 @@
+package com.lms.lmsdesktop.faculty;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FacultyLMS extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
