a74a8fa53f5527926e8a241a8adeaea327dcd392
FahimFBA <fahimbinamin@gmail.com>
2023-04-19 01:48:59 +0600

    FacultyLMS boilerplate

--- src/main/java/com/lms/lmsdesktop/faculty/FacultyLMS.java
+++ src/main/java/com/lms/lmsdesktop/faculty/FacultyLMS.java
@@ -1,11 +1,29 @@
 package com.lms.lmsdesktop.faculty;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class FacultyLMS extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
+import java.io.IOException;
+import java.net.URL;
 
+public class FacultyLMS extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Load the FXML file from the specified path
+        URL fxmlUrl = getClass().getResource("faculty-lms-view.fxml");
+        Parent root = FXMLLoader.load(fxmlUrl);
+
+        // Set up the scene and show the stage
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Faculty LMS");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
