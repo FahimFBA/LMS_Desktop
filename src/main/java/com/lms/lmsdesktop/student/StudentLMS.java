e2e7aad5b8aa602814513192e046d2088acfcd83
FahimFBA <fahimbinamin@gmail.com>
2023-04-19 13:07:34 +0600

    ubfinished

--- src/main/java/com/lms/lmsdesktop/student/StudentLMS.java
+++ src/main/java/com/lms/lmsdesktop/student/StudentLMS.java
@@ -6,15 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import java.io.IOException;
 import java.net.URL;
+import java.io.IOException;
 
 public class StudentLMS extends Application {
-    double x,y = 0;
+    double x, y = 0;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("student-lms-view.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/lms/lmsdesktop/student/student-lms-view.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
         root.setOnMousePressed(event -> {
@@ -27,12 +27,19 @@ public class StudentLMS extends Application {
             primaryStage.setY(event.getScreenY() - y);
         });
 
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        URL cssUrl = getClass().getResource("/com/lms/lmsdesktop/student/stylesheets/Style.css");
+        if (cssUrl != null) {
+            scene.getStylesheets().add(cssUrl.toExternalForm());
+        } else {
+            System.err.println("CSS file not found: /com/lms/lmsdesktop/student/stylesheets/Style.css");
+        }
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
